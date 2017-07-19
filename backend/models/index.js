c3eb7c22226359b4b2c6af21e7b7d70051f25e59
bryan <bryanbethea28@gmail.com>
2017-07-19 17:13:22 -0400

    works

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../../config.json')[env];
+var config    = require(__dirname + '../../config.json')[env];
 var db        = {};
 
 if (process.env.DATABASE_URL) {
@@ -18,7 +18,7 @@ if (process.env.DATABASE_URL) {
     logging: true, //false
   });
 } else {
-  var config = require(__dirname + '/../../config.json')[env];
+  var config = require(__dirname + '../../config.json')[env];
   if(config.use_env_variable) {
     var sequelize = new Sequelize(process.env[config.use_env_variable]);
   } else {
